4d7cbfb397dd97ddfefff3c72396689f50fd7b30
dalienst <oduordalienst@gmail.com>
2024-12-27 18:04:01 +0300

    added field to reports

--- reports/serializers.py
+++ reports/serializers.py
@@ -19,6 +19,7 @@ class ReportSerializer(serializers.ModelSerializer):
             "report_type",
             "title",
             "description",
+            "is_solved",
             "created_at",
             "updated_at",
             "slug",
