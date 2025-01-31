56878ec7c757f58151d8d46364a90915d0d73ca4
dalienst <oduordalienst@gmail.com>
2025-01-31 09:17:04 +0300

    added model

--- dailymassreading/admin.py
+++ dailymassreading/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from dailymassreading.models import DailyMassReading
+
+admin.site.register(DailyMassReading)
--- dailymassreading/models.py
+++ dailymassreading/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from accounts.abstracts import TimeStampedModel, UniversalIdModel, ReferenceSlugModel
+
+
+class DailyMassReading(TimeStampedModel, UniversalIdModel, ReferenceSlugModel):
+    title = models.CharField(max_length=2555)
+    lectionary = models.CharField(max_length=255)
+    reading_one = models.CharField(max_length=255)
+    reading_one_text = models.TextField()
+    psalm = models.CharField(max_length=255)
+    responsorial_psalm = models.TextField()
+    reading_two = models.CharField(max_length=255, blank=True, null=True)
+    reading_two_text = models.TextField(blank=True, null=True)
+    alleluia = models.CharField(max_length=255, blank=True, null=True)
+    alleluia_text = models.TextField(blank=True, null=True)
+    gospel = models.CharField(max_length=255)
+    gospel_text = models.TextField()
+
+    class Meta:
+        verbose_name = "Daily Mass Reading"
+        verbose_name_plural = "Daily Mass Readings"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
