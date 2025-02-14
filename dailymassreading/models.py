1a00280dc61fe93e4d481b10a2e39dafbb0d45d7
dalienst <oduordalienst@gmail.com>
2025-02-14 11:22:16 +0300

    added ordering

--- dailymassreading/models.py
+++ dailymassreading/models.py
@@ -21,7 +21,7 @@ class DailyMassReading(TimeStampedModel, UniversalIdModel, ReferenceSlugModel):
     class Meta:
         verbose_name = "Daily Mass Reading"
         verbose_name_plural = "Daily Mass Readings"
-        ordering = ["-mass_date"]
+        ordering = ["-mass_date", "-created_at"]
 
     def __str__(self):
         return self.title
