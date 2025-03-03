df243bebd39043a9cfc3c0c0f9de5a7e6dfd78bb
dalienst <oduordalienst@gmail.com>
2025-03-03 17:41:06 +0300

    add signals to app

--- earlyaccess/apps.py
+++ earlyaccess/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class EarlyaccessConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'earlyaccess'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "earlyaccess"
+
+    def ready(self):
+        import earlyaccess.signals
